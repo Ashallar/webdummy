3b004f14f226ecde738e6414689a0bd9b80142b7
Bastien Debret <bastien.debret@dispolink.fr>
2018-04-24 18:13:45 +0200

    PDF generation

--- WebDummy/Controllers/HomeController.cs
+++ WebDummy/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -30,6 +34,38 @@ namespace WebDummy.Controllers
         public ActionResult TestSmoothScrollUp()
         {
             return View();
+        }
+
+        public ActionResult GeneratePDFInvoice()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public FileResult GeneratePDF(string productname, double productquantity)
+        {
+            System.Diagnostics.Debug.WriteLine($"Name: {productname} Quantity: {productquantity}");
+
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "Created with PDFSHarp";
+
+            PdfPage page = document.AddPage();
+
+            // Get an XGraphics object for drawing
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+
+            XFont font = new XFont("Verdana", 20, XFontStyle.BoldItalic);
+            gfx.DrawString("Hello, World!", font, XBrushes.Black,
+            new XRect(0, 0, page.Width, page.Height),
+            XStringFormats.Center);
+            const string filename = "HelloWorld.pdf";
+
+            MemoryStream stream = new MemoryStream();
+            document.Save(stream, false);
+            byte[] bytes = stream.ToArray();
+
+            return File(bytes, "application/pdf");
         }
     }
 }
